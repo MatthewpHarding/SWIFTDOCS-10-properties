02cc86f994efff17d2e415b1fd96751951380847
Matthew Harding <matthewpharding@gmail.com>
2022-10-16 11:08:33 +0100

    Added the page number (from official Swift documentation) to each Xcode playground file.

--- Properties.playground/Pages/Properties.xcplaygroundpage/Contents.swift
+++ Properties.playground/Pages/Properties.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: ![Swift](swift-logo.png)
 //: Swift v5.7 [official docs](https://docs.swift.org/swift-book/LanguageGuide/Properties.html)
-//: # Properties
+//: # 10. Properties
 //:
 //:
 //: Properties associate values with a particular class, structure, or enumeration. Stored properties store constant and variable values as part of an instance, whereas computed properties calculate (rather than store) a value. Computed properties are provided by classes, structures, and enumerations. Stored properties are provided only by classes and structures.
